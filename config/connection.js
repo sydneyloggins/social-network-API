0758da6e0986a762f693c4d2bf819c5402e0b6c8
Sydney Loggins <sydneyloggins@Sydneys-Mini.attlocal.net>
2023-07-18 10:21:27 -0500

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+);
+
+mongoose.set('debug', true);
+
+module.exports = mongoose.connection;
---
+++ models/Thought.js
@@ -0,0 +1,73 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const { time } = require('console');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: 'Reaction is Required',
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Thought is Required',
+            minlength: 1,
+            maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
